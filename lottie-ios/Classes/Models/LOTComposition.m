fec4ec176551df659eb844dee8ec8886b8357198
Fredrik Littmarck <dreadrik@gmail.com>
2017-05-19 12:31:13 +0200

    Fixed incorrect NSAsserts in _buildAnimationForKeyframes() methods.

--- lottie-ios/Classes/Models/LOTComposition.m
+++ lottie-ios/Classes/Models/LOTComposition.m
@@ -34,7 +34,7 @@
   _framerate = [jsonDictionary[@"fr"] copy];
   
   if (_startFrame && _endFrame && _framerate) {
-    NSInteger frameDuration = _endFrame.integerValue - _startFrame.integerValue;
+    NSInteger frameDuration = _endFrame.floatValue - _startFrame.floatValue;
     NSTimeInterval timeDuration = frameDuration / _framerate.floatValue;
     _timeDuration = timeDuration;
   }
